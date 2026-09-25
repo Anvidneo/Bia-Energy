c9d12c3c5fe94f242104be1ef8bebe3942cce6a1
Anvidneo <Botero1400@gmail.com>
2026-09-25 01:29:23 +0000

    chore: enforce Conventional Commits with commitlint + husky

---
+++ commitlint.config.cjs
@@ -0,0 +1,3 @@
+// Enforces Conventional Commits (https://www.conventionalcommits.org) on
+// every commit in this repo via the .husky/commit-msg hook below.
+module.exports = { extends: ['@commitlint/config-conventional'] };
